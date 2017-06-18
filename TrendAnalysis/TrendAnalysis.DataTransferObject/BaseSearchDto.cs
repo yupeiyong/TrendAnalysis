b5e71d2fefaecc1a571810105dc858eea65f8f13
yupeiyong <2758318132@qq.com>
2017-06-18 23:05:13 +0800

    添加记录服务类的测试

---
+++ TrendAnalysis/TrendAnalysis.DataTransferObject/BaseSearchDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendAnalysis.DataTransferObject
+{
+    public class BaseSearchDto
+    {
+        public int StartIndex { get; set; }
+
+        public int TakeCount { get; set; }
+    }
+}
---
+++ TrendAnalysis/TrendAnalysis.Service.Test/UnitTestMarkSixReportService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Transactions;
+using TrendAnalysis.DataTransferObject;
+
+namespace TrendAnalysis.Service.Test
+{
+    [TestClass]
+    public class UnitTestMarkSixReportService
+    {
+        [TestMethod]
+        public void TestSearch()
+        {
+            using(new TransactionScope())
+            {
+                var service = new MarkSixReportService();
+                var rows=service.Search(new MarkSixReportSearchDto { StartIndex=0,TakeCount=20});
+                Assert.IsTrue(rows.Count > 0);
+            }
+        }
+    }
+}
