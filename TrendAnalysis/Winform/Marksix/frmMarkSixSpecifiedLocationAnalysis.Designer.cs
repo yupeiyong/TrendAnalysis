f5b8b436dd56bada9ed83ecbd9f20d1bfd88228b
yupeiyong <2758318132@qq.com>
2017-07-09 23:22:13 +0800

    修改分析窗体布局

--- TrendAnalysis/Winform/Marksix/frmMarkSixSpecifiedLocationAnalysis.Designer.cs
+++ TrendAnalysis/Winform/Marksix/frmMarkSixSpecifiedLocationAnalysis.Designer.cs
@@ -29,161 +29,191 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cboNumberLocation = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.cboTimes = new System.Windows.Forms.ComboBox();
+            this.btnAnalysis = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.myDataGridView1 = new Winform.Common.myDataGridView();
+            this.tbBeforeTimesCount = new System.Windows.Forms.TextBox();
+            this.btnAnalysisBefore = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.myDataGridView1)).BeginInit();
+            this.btnPurchase = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvBeforeAnalysis = new Winform.Common.myDataGridView();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBeforeAnalysis)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 37);
+            this.label1.Location = new System.Drawing.Point(22, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "位置：";
             // 
-            // comboBox1
+            // cboNumberLocation
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(110, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 26);
-            this.comboBox1.TabIndex = 1;
+            this.cboNumberLocation.FormattingEnabled = true;
+            this.cboNumberLocation.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.cboNumberLocation.Location = new System.Drawing.Point(79, 39);
+            this.cboNumberLocation.Name = "cboNumberLocation";
+            this.cboNumberLocation.Size = new System.Drawing.Size(121, 26);
+            this.cboNumberLocation.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(263, 40);
+            this.label2.Location = new System.Drawing.Point(254, 43);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.Size = new System.Drawing.Size(53, 18);
             this.label2.TabIndex = 0;
-            this.label2.Text = "期次";
+            this.label2.Text = "期次:";
             // 
-            // comboBox2
+            // cboTimes
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(342, 40);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 26);
-            this.comboBox2.TabIndex = 1;
+            this.cboTimes.FormattingEnabled = true;
+            this.cboTimes.Location = new System.Drawing.Point(310, 39);
+            this.cboTimes.Name = "cboTimes";
+            this.cboTimes.Size = new System.Drawing.Size(121, 26);
+            this.cboTimes.TabIndex = 1;
             // 
-            // button1
+            // btnAnalysis
             // 
-            this.button1.Location = new System.Drawing.Point(501, 42);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 38);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "分析";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAnalysis.Location = new System.Drawing.Point(478, 33);
+            this.btnAnalysis.Name = "btnAnalysis";
+            this.btnAnalysis.Size = new System.Drawing.Size(80, 38);
+            this.btnAnalysis.TabIndex = 2;
+            this.btnAnalysis.Text = "分析";
+            this.btnAnalysis.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // tbResult
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 95);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(547, 316);
-            this.textBox1.TabIndex = 3;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(34, 438);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "购买";
-            this.button2.UseVisualStyleBackColor = true;
+            this.tbResult.Location = new System.Drawing.Point(20, 84);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(547, 360);
+            this.tbResult.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(46, 39);
+            this.label3.Location = new System.Drawing.Point(18, 42);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.Size = new System.Drawing.Size(116, 18);
             this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.label3.Text = "当前期次以上";
             // 
-            // textBox2
+            // tbBeforeTimesCount
             // 
-            this.textBox2.Location = new System.Drawing.Point(129, 34);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 28);
-            this.textBox2.TabIndex = 5;
+            this.tbBeforeTimesCount.Location = new System.Drawing.Point(133, 37);
+            this.tbBeforeTimesCount.Name = "tbBeforeTimesCount";
+            this.tbBeforeTimesCount.Size = new System.Drawing.Size(100, 28);
+            this.tbBeforeTimesCount.TabIndex = 5;
             // 
-            // button3
+            // btnAnalysisBefore
             // 
-            this.button3.Location = new System.Drawing.Point(306, 39);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "分析";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // myDataGridView1
-            // 
-            this.myDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.myDataGridView1.Location = new System.Drawing.Point(61, 92);
-            this.myDataGridView1.Name = "myDataGridView1";
-            this.myDataGridView1.RowTemplate.Height = 30;
-            this.myDataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.myDataGridView1.TabIndex = 7;
+            this.btnAnalysisBefore.Location = new System.Drawing.Point(310, 36);
+            this.btnAnalysisBefore.Name = "btnAnalysisBefore";
+            this.btnAnalysisBefore.Size = new System.Drawing.Size(75, 31);
+            this.btnAnalysisBefore.TabIndex = 6;
+            this.btnAnalysisBefore.Text = "分析";
+            this.btnAnalysisBefore.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.cboNumberLocation);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.tbResult);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Controls.Add(this.cboTimes);
+            this.groupBox1.Controls.Add(this.btnPurchase);
+            this.groupBox1.Controls.Add(this.btnAnalysis);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(635, 646);
+            this.groupBox1.Size = new System.Drawing.Size(591, 646);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "分析指定位置号码";
             // 
             // groupBox2
             // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.textBox2);
-            this.groupBox2.Controls.Add(this.myDataGridView1);
-            this.groupBox2.Controls.Add(this.button3);
-            this.groupBox2.Location = new System.Drawing.Point(668, 24);
+            this.groupBox2.Controls.Add(this.tbBeforeTimesCount);
+            this.groupBox2.Controls.Add(this.dgvBeforeAnalysis);
+            this.groupBox2.Controls.Add(this.btnAnalysisBefore);
+            this.groupBox2.Location = new System.Drawing.Point(621, 13);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(0);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(764, 634);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox2.Size = new System.Drawing.Size(777, 646);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "指定位置号码以前期次分析";
+            // 
+            // btnPurchase
+            // 
+            this.btnPurchase.Location = new System.Drawing.Point(20, 458);
+            this.btnPurchase.Name = "btnPurchase";
+            this.btnPurchase.Size = new System.Drawing.Size(80, 38);
+            this.btnPurchase.TabIndex = 2;
+            this.btnPurchase.Text = "购买";
+            this.btnPurchase.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "期";
+            // 
+            // dgvBeforeAnalysis
+            // 
+            this.dgvBeforeAnalysis.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvBeforeAnalysis.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBeforeAnalysis.Location = new System.Drawing.Point(21, 84);
+            this.dgvBeforeAnalysis.Name = "dgvBeforeAnalysis";
+            this.dgvBeforeAnalysis.RowTemplate.Height = 30;
+            this.dgvBeforeAnalysis.Size = new System.Drawing.Size(730, 547);
+            this.dgvBeforeAnalysis.TabIndex = 7;
             // 
             // frmMarkSixSpecifiedLocationAnalysis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(1544, 670);
+            this.ClientSize = new System.Drawing.Size(1440, 670);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmMarkSixSpecifiedLocationAnalysis";
             this.Text = "MarkSix指定位置号码分析";
-            ((System.ComponentModel.ISupportInitialize)(this.myDataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBeforeAnalysis)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -191,17 +221,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cboNumberLocation;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cboTimes;
+        private System.Windows.Forms.Button btnAnalysis;
+        private System.Windows.Forms.TextBox tbResult;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button3;
-        private Common.myDataGridView myDataGridView1;
+        private System.Windows.Forms.TextBox tbBeforeTimesCount;
+        private System.Windows.Forms.Button btnAnalysisBefore;
+        private Common.myDataGridView dgvBeforeAnalysis;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnPurchase;
+        private System.Windows.Forms.Label label4;
     }
 }
