ddccd837356214ea8f7c3a0dd9a188fdb56974b0
yupeiyong <2758318132@qq.com>
2017-08-07 23:43:42 +0800

    添加组合类

---
+++ TrendAnalysis/TrendAnalysis.Service/Combination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendAnalysis.Service
+{
+    public class Combination
+    {
+    }
+    public class Node<T>
+    {
+        public List<T> Left { get; set; }
+        public List<T> Right { get; set; }
+    }
+}
