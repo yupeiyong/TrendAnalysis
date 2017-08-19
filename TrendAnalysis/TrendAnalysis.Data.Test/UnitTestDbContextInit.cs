c47f17daa0cd4c67e5318c4fa19b3dd276d3fb89
yupeiyong <2758318132@qq.com>
2017-08-19 16:34:41 +0800

    修改文件名

---
+++ TrendAnalysis/TrendAnalysis.Data.Test/UnitTestDbContextInit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrendAnalysis.Models;
+
+namespace TrendAnalysis.Data.Test
+{
+    [TestClass]
+    public class UnitTestDbContextInit
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using(var dao=new TrendDbContext())
+            {
+                dao.Set<MarkSixRecord>().Add(new MarkSixRecord {Times="2017004",FirstNum=12,AwardingDate=DateTime.Now });
+                dao.SaveChanges();
+            }
+        }
+    }
+}
