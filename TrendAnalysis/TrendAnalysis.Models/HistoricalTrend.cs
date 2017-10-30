b3cfc52b7e66b7da73be55cca402140500e8e128
yupeiyong <2758318132@qq.com>
2017-10-31 00:39:50 +0800

    add Historical trends

---
+++ TrendAnalysis/TrendAnalysis.Models/HistoricalTrend.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendAnalysis.Models
+{
+    /// <summary>
+    /// 历史趋势
+    /// </summary>
+    public class HistoricalTrend<T>
+    {
+        /// <summary>
+        /// 第几位数
+        /// </summary>
+        public int Location { get; set; }
+
+
+        /// <summary>
+        /// 允许的连续次数
+        /// </summary>
+        public int AllowConsecutiveTimes { set; get; }
+
+
+        /// <summary>
+        /// 允许的间隔数
+        /// </summary>
+        public int AllowInterval { get; set; }
+
+
+        /// <summary>
+        /// 正确次数
+        /// </summary>
+        public int CorrectCount { get; set; }
+
+
+        /// <summary>
+        /// 有分析结果的次数（符合连续次数和间隔数的记录次数）
+        /// </summary>
+        public int AnalyticalCount { get; set; }
+
+
+        /// <summary>
+        /// 总期次
+        /// </summary>
+        public int TotalHistoricalCount { get; set; }
+
+
+        /// <summary>
+        /// 正确率
+        /// </summary>
+        public double CorrectRate => AnalyticalCount == 0 ? 0 : (double)CorrectCount / AnalyticalCount;
+
+        public List<HistoricalTrendItem<T>> Items { get; set; }
+
+        public override string ToString()
+        {
+            var content = new StringBuilder();
+            content.AppendLine(string.Format("第{0}位，允许连续次数{1},允许间隔数{2}。出现次数{3}，正确次数{4},正确率：{5:0.00%}", Location, AllowConsecutiveTimes, AllowInterval, AnalyticalCount, CorrectCount, CorrectRate));
+            if (Items != null && Items.Count > 0)
+            {
+                foreach (var item in Items)
+                {
+                    var message = string.Format("期次：{0},号码：{1},分析结果：{4},结果连续次数:{5}结果间隔数：{6}", item.Times, item.Number, item.Success ? "-Yes- " : "-No-  ", item.ResultConsecutiveTimes, item.ResultInterval);
+                    content.AppendLine(message + (item.OppositeFactor != null ? string.Join(";", item.OppositeFactor) : ""));
+                }
+            }
+            return content.ToString();
+        }
+    }
+}
---
+++ TrendAnalysis/TrendAnalysis.Models/HistoricalTrendItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendAnalysis.Models
+{
+    /// <summary>
+    /// 历史趋势明细项
+    /// </summary>
+    public class HistoricalTrendItem<T>
+    {
+        /// <summary>
+        /// 期次
+        /// </summary>
+        public string Times { get; set; }
+
+        /// <summary>
+        /// 号码
+        /// </summary>
+        public T Number;
+
+        /// <summary>
+        /// 连续次数
+        /// </summary>
+        public int ResultConsecutiveTimes { set; get; }
+
+
+        /// <summary>
+        /// 间隔数
+        /// </summary>
+        public int ResultInterval { get; set; }
+
+
+        /// <summary>
+        /// 是否正确
+        /// </summary>
+        public bool Success { get; set; }
+
+
+        public List<T> OppositeFactor { get; set; }
+    }
+}
