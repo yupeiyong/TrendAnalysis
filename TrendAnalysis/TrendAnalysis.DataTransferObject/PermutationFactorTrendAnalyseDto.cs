5f4fadcd7a5687c287f74c84844347763fcca676
yupeiyong <2758318132@qq.com>
2017-12-20 10:34:54 +0800

    单元测试，统计连续次数

--- TrendAnalysis/TrendAnalysis.DataTransferObject/PermutationFactorTrendAnalyseDto.cs
+++ TrendAnalysis/TrendAnalysis.DataTransferObject/PermutationFactorTrendAnalyseDto.cs
@@ -1,47 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using TrendAnalysis.Models.Trend;
+
 
 namespace TrendAnalysis.DataTransferObject
 {
+
     public class PermutationFactorTrendAnalyseDto<T>
     {
+
         /// <summary>
-        /// 历史记录
+        ///     历史记录
         /// </summary>
         public List<T> Numbers { get; set; }
 
 
         /// <summary>
-        /// 排列因子
+        ///     排列因子
         /// </summary>
         public List<List<Factor<T>>> PermutationFactors { get; set; }
 
 
         /// <summary>
-        /// 允许最大的间隔数（最大连续期数-指定期次此因子连续次数）
+        ///     允许最大的间隔数（最大连续期数-指定期次此因子连续次数）
         /// </summary>
         public int AllowMaxInterval { get; set; } = int.MaxValue;
 
 
         /// <summary>
-        /// 分析集合时，允许的最小连续数，大于等于此数才记录连续次数
+        ///     分析集合时，允许的最小连续数，大于等于此数才记录连续次数
         /// </summary>
         public int AllowMinTimes { get; set; } = 1;
 
 
         /// <summary>
-        /// 允许的最小指定期次此因子连续次数
+        ///     允许的最小指定期次此因子连续次数
         /// </summary>
         public int AllowMinFactorCurrentConsecutiveTimes { get; set; } = 1;
 
 
         /// <summary>
-        /// 记录尾部切去数量，比如原长度100，切去10，最终保留90
+        ///     记录尾部切去数量，比如原长度100，切去10，最终保留90
         /// </summary>
         public int NumbersTailCutCount { get; set; }
+
     }
+
 }
--- TrendAnalysis/TrendAnalysis.Service.Test/Trend/UnitTestPermutationFactorTrend.cs
+++ TrendAnalysis/TrendAnalysis.Service.Test/Trend/UnitTestPermutationFactorTrend.cs
@@ -1,39 +1,111 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TrendAnalysis.Service.Trend;
 using TrendAnalysis.Models.Trend;
-using System.Collections.Generic;
+using TrendAnalysis.Service.Trend;
+
 
 namespace TrendAnalysis.Service.Test.Trend
 {
+
     [TestClass]
     public class UnitTestPermutationFactorTrend
     {
+
         [TestMethod]
         public void TestPermutationFactorTrendAnalyseResult_Two_Element()
         {
-            var fac1 = new Factor<int> { Left = new List<int> { 1, 2 }, Right = new List<int> { 3, 4 } };
-            var fac2 = new Factor<int> { Left = new List<int> { 5, 6 }, Right = new List<int> { 7, 8 } };
-            var ls = new List<Factor<int>> { fac1, fac2 };
+            var fac1 = new Factor<int> {Left = new List<int> {1, 2}, Right = new List<int> {3, 4}};
+            var fac2 = new Factor<int> {Left = new List<int> {5, 6}, Right = new List<int> {7, 8}};
+            var ls = new List<Factor<int>> {fac1, fac2};
 
-            var lss = new List<List<Factor<int>>> { ls, ls };
-            var result = PermutationFactorTrend.TraversePermutationFactor<int>(lss);
-            Assert.IsTrue(result.Count == 4 * 4);
+            var lss = new List<List<Factor<int>>> {ls, ls};
+            var result = PermutationFactorTrend.TraversePermutationFactor(lss);
+            Assert.IsTrue(result.Count == 4*4);
         }
 
 
         [TestMethod]
         public void TestPermutationFactorTrendAnalyseResult_One_And_Two_Element()
         {
-            var fac1 = new Factor<int> { Left = new List<int> { 1, 2 }, Right = new List<int> { 3, 4 } };
-            var fac2 = new Factor<int> { Left = new List<int> { 5, 6 }, Right = new List<int> { 7, 8 } };
-            var ls1 = new List<Factor<int>> { fac1 };
-            var ls = new List<Factor<int>> { fac1, fac2 };
+            var fac1 = new Factor<int> {Left = new List<int> {1, 2}, Right = new List<int> {3, 4}};
+            var fac2 = new Factor<int> {Left = new List<int> {5, 6}, Right = new List<int> {7, 8}};
+            var ls1 = new List<Factor<int>> {fac1};
+            var ls = new List<Factor<int>> {fac1, fac2};
 
-            var lss = new List<List<Factor<int>>> { ls1, ls };
-            var result = PermutationFactorTrend.TraversePermutationFactor<int>(lss);
-            Assert.IsTrue(result.Count == 2 * 4);
+            var lss = new List<List<Factor<int>>> {ls1, ls};
+            var result = PermutationFactorTrend.TraversePermutationFactor(lss);
+            Assert.IsTrue(result.Count == 2*4);
+        }
+
+
+        [TestMethod]
+        public void TestCountConsecutive()
+        {
+            var numbers = new List<byte>
+            {
+                1, 3, 6, 9, 1, 4, 2, 3, 1, 2, 5, 6, 8, 2, 3, 1
+            };
+
+            var factors = new List<List<byte>>
+            {
+                new List<byte> {1, 2},
+                new List<byte> {3, 4}
+            };
+
+            var oppositeFactor = new List<byte> {5, 6};
+
+            var cutCount = 6;
+            var result = PermutationFactorTrend.CountConsecutive(numbers, factors, oppositeFactor, cutCount);
+
+            //有两个连续次数
+            Assert.IsTrue(result.HistoricalConsecutiveTimes.Count == 2);
+
+            //连续次数＝1出现一次
+            Assert.IsTrue(result.HistoricalConsecutiveTimes[1] == 1);
+
+            //连续次数＝2出现一次
+            Assert.IsTrue(result.HistoricalConsecutiveTimes[2] == 1);
+
+
+            numbers = new List<byte>
+            {
+                1, 3, 9, 1, 3, 1, 3, 1, 3, 6, 9, 1, 4, 2, 3, 1, 4, 5, 6, 8, 2, 3, 1
+            };
+
+            result = PermutationFactorTrend.CountConsecutive(numbers, factors, oppositeFactor, cutCount);
+
+            //有两个连续次数
+            Assert.IsTrue(result.HistoricalConsecutiveTimes.Count == 2);
+
+            //连续次数＝1出现一次
+            Assert.IsTrue(result.HistoricalConsecutiveTimes[1] == 1);
+
+            //连续次数＝3出现2次
+            Assert.IsTrue(result.HistoricalConsecutiveTimes[3] == 2);
+
+
+
+            numbers = new List<byte>
+            {
+                1, 3, 9, 1, 3, 1, 3, 1, 3, 6, 9, 1, 4, 2, 3, 1, 4,1, 4, 2, 3, 1, 4,1, 4, 2, 3, 1, 4, 5, 6, 8, 2, 3, 1
+            };
+
+            result = PermutationFactorTrend.CountConsecutive(numbers, factors, oppositeFactor, cutCount);
+
+            //有两个连续次数
+            Assert.IsTrue(result.HistoricalConsecutiveTimes.Count == 3);
+
+            //连续次数＝1出现一次
+            Assert.IsTrue(result.HistoricalConsecutiveTimes[1] == 1);
+
+            //连续次数＝3出现一次
+            Assert.IsTrue(result.HistoricalConsecutiveTimes[3] == 1);
+
+            //连续次数＝9出现一次
+            Assert.IsTrue(result.HistoricalConsecutiveTimes[9] == 1);
+
         }
 
     }
+
 }
