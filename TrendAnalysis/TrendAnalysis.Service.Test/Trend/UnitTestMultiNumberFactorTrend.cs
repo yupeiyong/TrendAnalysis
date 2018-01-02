21fb7b50b4ed68cae11ca09fe6bd67863ab55339
yupeiyong <2758318132@qq.com>
2018-01-03 00:25:45 +0800

    test MultiNumberFactorTrend  TestAnalyse

--- TrendAnalysis/TrendAnalysis.Service.Test/Trend/UnitTestMultiNumberFactorTrend.cs
+++ TrendAnalysis/TrendAnalysis.Service.Test/Trend/UnitTestMultiNumberFactorTrend.cs
@@ -15,10 +15,10 @@ namespace TrendAnalysis.Service.Test.Trend
         [TestMethod]
         public void TestAnalyseConsecutives()
         {
-            var maxCount = 1;
+            var maxCount = 2;
             var dto = new MultiNumberFactorTrendAnalyseDto<byte>
             {
-                Numbers = new List<byte>() { 1, 3, 9, 3, 8, 4, 0, 1, 9, 6, 5, 4, 6, 3, 0, 0, 6, 6, 9, 9, 2, 2, 4, 4, 0 },
+                Numbers = new List<byte>() { 1, 3, 9, 3, 8, 4, 7, 1, 9, 6, 4, 4, 6, 3, 2, 3, 6, 3, 3, 9, 2, 2, 4, 4, 0 },
                 Factors = new List<Factor<byte>>() { new Factor<byte>() { Left = new List<byte>() { 1, 2 }, Right = new List<byte>() { 3, 4 } } },
                 AllowMaxInterval = 0,
                 AllowMinFactorCurrentConsecutiveTimes = 2,
@@ -34,5 +34,51 @@ namespace TrendAnalysis.Service.Test.Trend
             };
             var resultList = MultiNumberFactorTrend.AnalyseConsecutives(dto);
         }
+
+        [TestMethod]
+        public void TestAnalyse()
+        {
+            var maxCount = 2;
+            var dto = new MultiNumberFactorTrendAnalyseDto<byte>
+            {
+                Numbers = new List<byte>() { 1, 3, 9, 3, 8, 4, 7, 1, 9, 6, 4, 4, 6, 3, 2, 3, 6, 3, 3, 9, 2, 2, 4, 4, 0 },
+                Factors = new List<Factor<byte>>() { new Factor<byte>() { Left = new List<byte>() { 1, 2 }, Right = new List<byte>() { 3, 4 } } },
+                AllowMaxInterval = 0,
+                AllowMinFactorCurrentConsecutiveTimes = 2,
+                AllowMinTimes = 1,
+                NumbersTailCutCount = 6,
+                AnalyseConsecutiveCompareFunc = (nums, factors, i) =>
+                {
+                    var sum = nums[i] + nums[i - 1];
+                    var curNumber = (byte)(sum % 10);
+                    return factors.Contains(curNumber);
+                },
+                MultiNumberMaxCount = maxCount,
+                PredictiveConsecutivesCompareFunc = (nums, factors, i) =>
+                 {
+                     var sum = nums[i] + nums[i + 1];
+                     var curNumber = (byte)(sum % 10);
+                     return factors.Contains(curNumber);
+                 },
+                PredictiveFactorAction = (nums, factor) =>
+                {
+                    //反向累加
+                    var currentSum = 0;
+                    var around =0;
+                    var lastIndex = nums.Count - 1;
+                    for (var n = around; n >=0; n--)
+                    {
+                        currentSum += nums[lastIndex - n];
+                    }
+                    //取10的模
+                    var sum = (byte)(currentSum % 10);
+
+                    //当前因子数-累加数取10的模（分析数字可能区域）
+                    factor.ForEach(f => { f = (byte)((f + 10 - sum) % 10); });
+                }
+            };
+            var resultList = new MultiNumberFactorTrend().Analyse(dto);
+        }
+
     }
 }
